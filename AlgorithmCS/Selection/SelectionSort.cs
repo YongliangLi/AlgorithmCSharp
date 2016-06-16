d8412d9bba9eb9d634ba3bf05e2d5ff814e61c12
U-AMERICAS\Yongliang_Li <Yongliang_Li@WN7X64-GT55C42.aus.amer.dell.com>
2016-06-16 17:20:39 -0500

    selection sort

---
+++ AlgorithmCS/Selection/SelectionSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection
+{
+    public class SelectionSort
+    {
+        public void Sort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                int min = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (Less(nums, i, j))
+                    {
+                        Swap(nums, i, j);
+                    }
+                }
+
+                Debug.Assert(IsSorted(nums,0,i));
+            }
+
+            Debug.Assert(IsSorted(nums));
+        }
+
+        private bool IsSorted(int[] nums)
+        {
+            return IsSorted(nums,0,nums.Length);
+        }
+
+        private bool IsSorted(int[] nums, int low, int high)
+        {
+            if (low < high)
+            {
+               Swap(ref low, ref high);
+            }
+
+            if (high-low == 0) return true;
+
+            for (int i = low+1; i <=high; i++)
+            {
+                if(Less(nums,i,i-1)) return false;
+            }
+
+            return true;
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        private bool Less(int[] nums, int i, int j)
+        {
+            if (nums[j] < nums[i]) return true;
+            return false;
+        }
+    }
+}
---
+++ AlgorithmCS/SelectionSort.Tests/BasicTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SelectionSort;
+
+namespace SelectionSort.Tests
+{
+    [TestClass]
+    public class BasicTest
+    {
+        private Selection.SelectionSort sorter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sorter = new Selection.SelectionSort();
+        }
+
+        [TestMethod]
+        public void ShouldSortNonsortInteger()
+        {
+            int[] data = {3, 1, 2};
+            sorter.Sort(data);
+
+            Assert.AreEqual(1,data[0]);
+            Assert.AreEqual(2, data[1]);
+            Assert.AreEqual(3, data[2]);
+        }
+
+        [TestMethod]
+        public void ShouldSortAscSortedInteger()
+        {
+            int[] data = { 1, 2, 3 };
+            sorter.Sort(data);
+
+            Assert.AreEqual(1, data[0]);
+            Assert.AreEqual(2, data[1]);
+            Assert.AreEqual(3, data[2]);
+        }
+
+        [TestMethod]
+        public void ShouldSortDescSortedInteger()
+        {
+            int[] data = { 3, 2, 1 };
+            sorter.Sort(data);
+
+            Assert.AreEqual(1, data[0]);
+            Assert.AreEqual(2, data[1]);
+            Assert.AreEqual(3, data[2]);
+        }
+
+        [TestMethod]
+        public void Should_return_empty_array()
+        {
+            int[] data = { };
+            sorter.Sort(data);
+
+            Assert.AreEqual(0,data.Length);
+        }
+
+        [TestMethod]
+        public void Should_sort_one_element_array()
+        {
+            int[] data = { 1 };
+            sorter.Sort(data);
+
+            Assert.AreEqual(1, data.Length);
+        }
+
+    }
+}
