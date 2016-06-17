afc202e04267aaa83eeab497b3eccc17a67b0b3d
Yongliang Li <Yongliang Li>
2016-06-16 22:00:18 -0500

    insertion sort

---
+++ AlgorithmCS/InsertionSort.Tests/InsertionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InsertionSort.Tests
+{
+    [TestClass]
+    public class InsertionTest
+    {
+        InsertionSorting sort = new InsertionSorting();
+
+        [TestMethod]
+        public void Should_Sort_array()
+        {
+            int[] data = { 6, 2, 3 };
+
+            sort.Sort(data);
+
+            Assert.AreEqual(data[0], 2);
+            Assert.AreEqual(data[1], 3);
+            Assert.AreEqual(data[2], 6);
+        }
+    }
+}
