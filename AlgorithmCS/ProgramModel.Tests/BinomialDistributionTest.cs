7d09a85e86938e6689cbad3dc4bfce83d6a8b765
yongliang_li <Yongliang_Li@DellTeam.com>
2016-06-17 12:00:34 -0500

    recursive binomial

---
+++ AlgorithmCS/ProgramModel.Tests/BinomialDistributionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgramModels;
+
+namespace ProgramModel.Tests
+{
+    [TestClass]
+    public class BinomialDistributionTest
+    {
+        BinomialDistribution bd=new BinomialDistribution();
+
+        [TestMethod]
+        public void Should_do_Recursive()
+        {
+            var result= bd.Binaomial(5, 2, .25);
+            Assert.AreEqual(result, 0.263671875);
+
+        }
+
+        [TestMethod]
+        public void Should_do_Loop()
+        {
+            var result = bd.Binomial2(5, 2, .25);
+            Assert.AreEqual(result, 0.263671875);
+
+        }
+
+    }
+}
---
+++ AlgorithmCS/ProgramModels/BinomialDistribution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramModels
+{
+    public class BinomialDistribution
+    {
+        public double Binaomial(int n, int k, double p)
+        {
+            if (n == 0 && k == 0) return 1.0;
+            if (n < 0 || k < 0) return 0.0;
+            return (1.0 - p)*Binaomial(n - 1, k, p) + p*Binaomial(n - 1, k - 1, p);
+        }
+
+        public double Binomial2(int n, int k, double p)
+        {
+            double[,] b = new double[n + 1, k + 1];
+            
+            //base case
+            for (int i = 0; i <= n; i++)
+            {
+                b[i, 0] = Math.Pow(1.0 - p, i);
+            }
+            b[0, 0] = 1.0;
+
+            //recursive formula
+            for (int i = 1; i <=n; i++)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    b[i, j] = p*b[i - 1, j - 1] + (1.0 - p)*b[i - 1, j];
+                }
+            }
+
+            return b[n, k];
+        }
+    }
+}
